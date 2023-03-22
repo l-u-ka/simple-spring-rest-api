6c6a609e1791b58862c0b6d7e5301a8691846c9e
Luka Kurtanidze <luka12208@gmail.com>
2023-03-22 21:20:52 +0400

    http method examples

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.Test;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @RequestMapping(value="/call", method= RequestMethod.GET, produces = {"application/json"})
+
+    public Test test() {
+        return new Test("Luka", "Kurtanidze");
+    }
+
+    @RequestMapping(value="/post", method = RequestMethod.POST, produces = {"application/json"})
+    public Test testPost(@RequestBody Test test) {
+        System.out.println(test.getFirstName() + " " + test.getLastName());
+        return new Test(test.getFirstName() + " from server", test.getLastName());
+    }
+
+}
